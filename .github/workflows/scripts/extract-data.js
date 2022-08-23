14195e143fccea003a3f9e0081daa92e0a558482
Chris Young <youngcm2@gmail.com>
2022-08-23 14:39:44 -0500

    fix: testing 3

---
+++ .github/workflows/scripts/extract-data.js
@@ -0,0 +1,18 @@
+module.exports = async ({ context, core }) => {
+  const { MAJOR, MINOR, PATCH, PR } = process.env;
+  const { sha, event } = context;
+
+  if (PR) {
+    const { title } = JSON.parse(PR);
+    const regex =
+      /(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)(-(?<preRelease>[^+]+))?(\+(?<build>.*))?/;
+    const matches = regex.exec(title);
+
+    core.info(matches);
+    core.setOutput("semver", matches[0]);
+    core.setOutput("short_sha", sha.substring(0, 7));
+  } else {
+    core.setOutput("semver", `${MAJOR}.${MINOR}.${PATCH}`);
+    core.setOutput("short_sha", event.before.substring(0, 7));
+  }
+};
